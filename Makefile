47e82d4af894f0d4bcc7fd2bfad8d3d00aa3394f
Infinidrix <biruksolomon11@gmail.com>
2021-08-19 13:28:10 +0300

    working kernel load and exec

--- Makefile
+++ Makefile
@@ -1,8 +1,16 @@
+build/os-image: build/boot/boot.bin build/kernel/kernel.bin
+	cat build/boot/boot.bin build/kernel/kernel.bin > build/os-image
+
 build/boot/boot.bin: build/boot/boot.o
 	ld -Ttext 0x7c00 --oformat binary -o build/boot/boot.bin build/boot/boot.o
 
-build/boot/boot.o: src/*/*.s src/*/*/*.s
+build/boot/boot.o: src/boot/*.s src/boot/*/*.s
 	as src/boot/boot.s -o build/boot/boot.o
 
-boot: build/boot/boot.bin
-	qemu-system-x86_64 -drive format=raw,file=build/boot/boot.bin,index=0,if=floppy
+build/kernel/kernel.bin: build/kernel/kernel.o
+	ld -o build/kernel/kernel.bin -Ttext 0x1000 build/kernel/kernel.o --oformat binary
+
+build/kernel/kernel.o: src/kernel/*.c
+	gcc -ffreestanding -c src/kernel/kernel.c -o build/kernel/kernel.o
+boot: build/os-image
+	qemu-system-x86_64 -drive format=raw,file=build/os-image,index=0,if=floppy
--- src/boot/boot.s
+++ src/boot/boot.s
@@ -5,39 +5,17 @@
 .globl _start;
 _start:                 #code entry point
     
-    pushw CODE_SEG
-    call print_hex
-    
-    movb $0x0e, %ah
-    movb $0x45, %al
-    int $0x10
-
-    movb $'A, %al
-    int $0x10
-
-    movb hello_msg, %al
-    int $0x10
-    
     movb %dl, BOOT_DRIVE
 
     movw $0x9000, %bp
     movw %bp, %sp
-     
-    pushw $0x9000
-    pushw BOOT_DRIVE
-    pushw $0x05
-    # call disk_load
 
     pushw $hello_msg
-    # call print_str
+    call print_str
+
+    call load_kernel
 
     call switch_to_pm
-
-
-    # pushw 0x9512
-    # call print_hex
-
-
 
     jmp hang 
 
@@ -48,20 +26,34 @@ _start:                 #code entry point
 .include "src/boot/utils-16/switch_to_pm.s"
 .include "src/boot/utils-32/print_str32.s"
 
+.code16
+load_kernel:
+    pushw %bp
+    movw %sp, %bp
+    pushw $0x1000
+    pushw BOOT_DRIVE
+    pushw $0x15
+    call disk_load
+    leave
+    ret
+
 .code32
 BEGIN_PM:
     pushl $new_msg
     call print_str32
+    call 0x1000
     jmp hang
 
 hello_msg:
-        .asciz "Hello There! General Kenobi!!!"
+    .asciz "Welcome to 16 bit land, hope you enjoy your transit"
 BOOT_DRIVE:
     .byte 0
 
 new_msg:
-    .asciz "There's a whole 32 bit world out there"
+    .asciz "  <- LOOK!! The start of something big. There's a whole 32 bit world out there"
 
+KERNEL_OFFSET:
+    .2byte 0x1000
 # Don't change anything below this
 hang:
     jmp hang
@@ -71,7 +63,3 @@ hang:
 # The magic number which tells BIOS that this is indeed OS code not junk
 magic:
     .2byte 0xaa55
-
-# .fill 256,2,0xdada
-# .fill 512,2,0xface
- 
