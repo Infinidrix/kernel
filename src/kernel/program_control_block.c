b14c4d4e46055872c3ecd862e7560b88b453a8d3
Infinidrix <biruksolomon11@gmail.com>
2021-09-17 13:28:04 +0300

    move pcb struct to header

--- src/kernel/program_control_block.c
+++ src/kernel/program_control_block.c
@@ -1,22 +1,2 @@
-#include "program_control_block.h";
+#include "program_control_block.h"
 
-struct PCB {
-    // process state
-    enum state process_state;
-
-    //process id
-    int id;
-
-    //program counter
-    int pc;
-
-    //registers
-    int eax;
-    int ecx;
-    int edx;
-    int ebx;
-    int esi;
-    int edi;
-    int esp;
-    int ebp;
-};
